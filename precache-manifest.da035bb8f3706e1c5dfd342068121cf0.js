a78d4f1e3f0bc9f3a0835fc506b0046d8a0f802a
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2019-11-21 01:40:39 +0200

    Updates

---
+++ precache-manifest.da035bb8f3706e1c5dfd342068121cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b81552ba7810135920ea83fdfbc6b5",
+    "url": "/Arabic-Quotes/index.html"
+  },
+  {
+    "revision": "49cd9b04f46edc47b9d5",
+    "url": "/Arabic-Quotes/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "67cde65ddc0a73a006c8",
+    "url": "/Arabic-Quotes/static/css/main.4102ae8a.chunk.css"
+  },
+  {
+    "revision": "49cd9b04f46edc47b9d5",
+    "url": "/Arabic-Quotes/static/js/2.659111b4.chunk.js"
+  },
+  {
+    "revision": "67cde65ddc0a73a006c8",
+    "url": "/Arabic-Quotes/static/js/main.f804ec8b.chunk.js"
+  },
+  {
+    "revision": "b315a3571ec682c44612",
+    "url": "/Arabic-Quotes/static/js/runtime-main.d119671c.js"
+  }
+]);
